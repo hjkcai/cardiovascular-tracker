450d7dc4fa285f443899a2cd0e16e7a0a6fb40dd
hjkcai <huajingkun1214@gmail.com>
2017-05-27 22:26:07 +0800

    feat(server): 读写 redis 时使用 JSON

--- server/src/lib/redis.ts
+++ server/src/lib/redis.ts
@@ -13,17 +13,17 @@ client.on('error', (err: any) => {
 export default client
 
 export function getRedis (key: string) {
-  return new Promise<string>((resolve, reject) => {
+  return new Promise<any>((resolve, reject) => {
     client.get(key, (err, result) => {
       if (err) reject(err)
-      else resolve(result)
+      else resolve(JSON.parse(result))
     })
   })
 }
 
-export function setRedis (key: string, value: string) {
+export function setRedis (key: string, value: any) {
   return new Promise<void>((resolve, reject) => {
-    client.set(key, value, err => {
+    client.set(key, JSON.stringify(value), err => {
       if (err) reject(err)
       else resolve()
     })
