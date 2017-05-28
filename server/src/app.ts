a21f713f29683a0914d97dde1444039e6b22350a
hjkcai <huajingkun1214@gmail.com>
2017-05-28 22:05:06 +0800

    fix(server): 未引入 bodyparser

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,16 @@
 'use strict'
 
 import './lib/db'
-import * as Koa from 'koa'
 import router from './routes'
+import * as Koa from 'koa'
+import * as bodyParser from 'koa-bodyparser'
 
 import ApiResponserMiddleware from './lib/middlewares/api-responser'
 import LoggerMiddleware from './lib/middlewares/logger'
 
 const app = new Koa()
 app.use(LoggerMiddleware)
+app.use(bodyParser())
 app.use(ApiResponserMiddleware)
 app.use(router.prefix('/api').routes())
 
