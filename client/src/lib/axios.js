98865766b335cc5071861770fc2686e586731d2e
hjkcai <huajingkun1214@gmail.com>
2017-05-28 22:51:26 +0800

    feat(client): 添加自定义 axios

---
+++ client/src/lib/axios.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import axios from 'axios'
+import wepyAxiosAdapter from 'wepy-plugin-axios/dist/adapter.js'
+
+const instance = axios.create({
+  adapter: wepyAxiosAdapter(axios),
+  baseURL: 'https://API_HOST/api',
+  headers: {
+    'Authorization': undefined
+  }
+})
+
+export default instance
+
+let token = ''
+export function getToken () { return token }
+export function setToken (newToken) {
+  token = newToken
+  instance.defaults.headers.common['Authorization'] = 'Bearer ' + token
+}
