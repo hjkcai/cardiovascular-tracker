4d05c78eee36ae952432afd17f3c0b42dfcbbf0b
hjkcai <huajingkun1214@gmail.com>
2017-05-28 19:45:28 +0800

    feat(server): 添加ssl选项

--- server/config.js
+++ server/config.js
@@ -4,12 +4,16 @@ const path = require('path')
 
 module.exports = {
   db: {
-    name: 'test',
+    name: 'cardiovascular-tracker',
     uri: 'mongodb://localhost'
   },
   logs: path.join(__dirname, 'logs'),
   port: 3000,
   secret: '',
+  ssl: {
+    key: '',
+    cert: ''
+  },
   wechat: {
     appId: '',
     appSecret: ''
