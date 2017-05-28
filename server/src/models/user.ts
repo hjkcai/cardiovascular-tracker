316112fcd66ae7fdcd0060c27162b720ac2e9bb0
hjkcai <huajingkun1214@gmail.com>
2017-05-28 19:38:07 +0800

    feat(server): 增加用户信息的储存内容

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -6,17 +6,40 @@ import { Document, Schema } from 'mongoose'
 export interface User extends Document {
   /** 等同于 openid */
   _id: string,
+
+  /** 生日 */
+  birthday: Date | null,
+
+  /** 身高 */
+  height: number | null,
+
+  /** 疾病情况 */
+  disease: string,
+
+  // 从微信获得的信息
   nickName: string,
   gender: 0 | 1 | 2,
   language: string,
   city: string,
   province: string,
   country: string,
-  avatarUrl: string
+  avatarUrl: string,
 }
 
 export const schema = new Schema({
   _id: String,
+  birthday: {
+    type: Date,
+    default: null
+  },
+  height: {
+    type: Number,
+    default: null
+  },
+  disease: {
+    type: String,
+    default: ''
+  },
   nickName: String,
   gender: Number,
   language: String,
