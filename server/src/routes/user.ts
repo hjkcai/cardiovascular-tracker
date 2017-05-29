9bfaa2c0c7fbe2740530f1f59db2cfe0567cd373
hjkcai <huajingkun1214@gmail.com>
2017-05-29 22:58:36 +0800

    feat(server): 添加用户信息路由

---
+++ server/src/routes/user.ts
@@ -0,0 +1,27 @@
+'use strict'
+
+import * as User from '../models/user'
+import * as Router from 'koa-router'
+import * as session from '../lib/session'
+
+const router = new Router()
+
+// 获取完整用户信息
+router.get('userinfo', async (ctx, next) => {
+  ctx.result = await User.getUserInfo(ctx.session.openid)
+})
+
+// 修改用户信息
+router.post('userinfo', async (ctx, next) => {
+  interface UserInfoData {
+    birthday: Date,
+    height: number,
+    disease: string,
+  }
+
+  const data: UserInfoData = ctx.request.body
+  await User.setUserInfo(ctx.session.openid, data)
+  ctx.result = null
+})
+
+export default router
