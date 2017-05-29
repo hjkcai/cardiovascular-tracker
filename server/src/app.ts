83ffa1bde5c70598b052bba33959a5d69303d48b
hjkcai <huajingkun1214@gmail.com>
2017-05-29 23:06:56 +0800

    feat(server): 添加 token 验证

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import router from './routes'
 import * as Koa from 'koa'
 import * as bodyParser from 'koa-bodyparser'
 
+import AuthMiddleware from './lib/middlewares/auth'
 import ApiResponserMiddleware from './lib/middlewares/api-responser'
 import LoggerMiddleware from './lib/middlewares/logger'
 
@@ -12,6 +13,7 @@ const app = new Koa()
 app.use(LoggerMiddleware)
 app.use(bodyParser())
 app.use(ApiResponserMiddleware)
+app.use(AuthMiddleware([/\/login$/]))
 app.use(router.prefix('/api').routes())
 
 export default app
