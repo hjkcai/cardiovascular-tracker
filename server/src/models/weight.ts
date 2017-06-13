4e4ddea78a888d27e218f3705f905899b9701847
hjkcai <huajingkun1214@gmail.com>
2017-06-13 10:00:18 +0800

    feat(server): 添加体重记录schema

---
+++ server/src/models/weight.ts
@@ -0,0 +1,36 @@
+'use strict'
+
+import db from '../lib/db'
+import { removeUndefined } from '../lib/util'
+import { Document, Schema } from 'mongoose'
+
+/** 体重记录 */
+export interface Weight extends Document {
+  /** 本条记录对应患者的 openid */
+  openid: string,
+
+  /** 体重值 */
+  value: number,
+
+  /** 体重记录时间 */
+  date: Date
+}
+
+export const schema = new Schema({
+  openid: {
+    type: String,
+    index: true,
+    required: true
+  },
+  value: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+})
+
+const model = db.model<Weight>('weight', schema)
+export default model
