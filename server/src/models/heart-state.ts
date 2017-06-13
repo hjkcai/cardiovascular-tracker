8ad8567cc5f627de84269ee34d39779906db197a
hjkcai <huajingkun1214@gmail.com>
2017-06-13 17:42:08 +0800

    feat(server): 添加心率血压的 schema

---
+++ server/src/models/heart-state.ts
@@ -0,0 +1,62 @@
+'use strict'
+
+import db from '../lib/db'
+import { Document, Schema } from 'mongoose'
+
+/** 心率血压记录 */
+export interface HeartState extends Document {
+  /** 本条记录对应患者的 openid */
+  openid: string,
+
+  /** 心率 */
+  heartRate: number,
+
+  /** 收缩压 */
+  systolic: number,
+
+  /** 舒张压 */
+  diastolic: number,
+
+  /** 心率血压记录时间 */
+  date: Date
+}
+
+export const schema = new Schema({
+  openid: {
+    type: String,
+    index: true,
+    required: true
+  },
+  heartRate: {
+    type: Number,
+    required: true
+  },
+  systolic: {
+    type: Number,
+    required: true
+  },
+  diastolic: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+})
+
+export const model = db.model<HeartState>('heart-state', schema)
+
+// WARNING: 代码与 ./weight.ts getWeightRecords 重复
+/** 获取某用户某时间范围的心率血压记录 */
+export function getHeartStateRecords (openid: string, from: Date, to: Date = new Date()) {
+  let $gte = from
+  let $lte = to
+
+  if (from > to) {
+    $gte = to
+    $lte = from
+  }
+
+  return model.find({ openid, date: { $gte, $lte } }, { openid: false }).sort({ date: -1 }).exec()
+}
