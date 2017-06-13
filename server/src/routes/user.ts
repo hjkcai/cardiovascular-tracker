e0c5176878dc90a5496ef083c2c544ebf3ba067b
hjkcai <huajingkun1214@gmail.com>
2017-06-13 11:14:41 +0800

    feat(server): 添加修改用户信息时的数据检查

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -2,6 +2,7 @@
 
 import * as User from '../models/user'
 import * as Router from 'koa-router'
+import { InvalidUserInputError } from '../lib/errors'
 
 const router = new Router()
 
@@ -15,10 +16,29 @@ router.post('userinfo', async (ctx, next) => {
   interface UserInfoData {
     birthday: Date,
     height: number,
-    disease: string[],
+    disease: string[]
   }
 
-  const data: UserInfoData = ctx.request.body
+  const data: Any<UserInfoData> = ctx.request.body
+
+  if (data.birthday != null) {
+    data.birthday = new Date(data.birthday)
+    if (Number.isNaN(data.birthday.valueOf())) {
+      throw new InvalidUserInputError('birthday', data.birthday)
+    }
+  }
+
+  if (data.height != null) {
+    data.height = Number.parseInt(data.height)
+    if (Number.isNaN(data.height)) {
+      throw new InvalidUserInputError('height', data.height)
+    }
+  }
+
+  if (data.disease != null && !Array.isArray(data.disease)) {
+    throw new InvalidUserInputError('disease', data.disease)
+  }
+
   await User.setUserInfo(ctx.session.openid, data)
   ctx.result = null
 })
