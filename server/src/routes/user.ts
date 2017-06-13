dfb3be1373e224dc0bba6abd20614b409d6a4c50
hjkcai <huajingkun1214@gmail.com>
2017-06-13 13:02:38 +0800

    feat(server): 使用验证器简化数据字段验证

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -2,7 +2,7 @@
 
 import * as User from '../models/user'
 import * as Router from 'koa-router'
-import { InvalidUserInputError } from '../lib/errors'
+import * as validators from '../lib/validators'
 
 const router = new Router()
 
@@ -19,25 +19,10 @@ router.post('userinfo', async (ctx, next) => {
     disease: string[]
   }
 
-  const data: Any<UserInfoData> = ctx.request.body
-
-  if (data.birthday != null) {
-    data.birthday = new Date(data.birthday)
-    if (Number.isNaN(data.birthday.valueOf())) {
-      throw new InvalidUserInputError('birthday', data.birthday)
-    }
-  }
-
-  if (data.height != null) {
-    data.height = Number.parseInt(data.height)
-    if (Number.isNaN(data.height)) {
-      throw new InvalidUserInputError('height', data.height)
-    }
-  }
-
-  if (data.disease != null && !Array.isArray(data.disease)) {
-    throw new InvalidUserInputError('disease', data.disease)
-  }
+  const data: UserInfoData = ctx.request.body
+  data.birthday = validators.validateDate('birthday', data)
+  data.disease = validators.validateArray('disease', data)
+  data.height = validators.validateNumber('height', data)
 
   await User.setUserInfo(ctx.session.openid, data)
   ctx.result = null
