93b38b848d2ea7958a75f64d9b1d4c538508b7a1
hjkcai <huajingkun1214@gmail.com>
2017-06-13 10:08:20 +0800

    fix(server): 数据库读写函数可能没有执行的潜在bug

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -54,18 +54,18 @@ const model = db.model<User>('user', schema)
 export default model
 
 /** 获取用户信息 */
-export function getUserInfo (openid: string): Promise<User> {
-  return model.findById(openid).then()
+export function getUserInfo (openid: string) {
+  return model.findById(openid).exec()
 }
 
 /** 修改用户信息 (只能修改不是从微信获取的用户信息) */
 export function setUserInfo (openid: string, { birthday, height, disease }: Partial<User>) {
   return model.findByIdAndUpdate(openid, {
     $set: removeUndefined({ birthday, height, disease })
-  })
+  }).exec()
 }
 
 /** 更新微信用户信息. 如果用户不存在则新建 */
 export function updateWechatUserInfo (userinfo: WechatUserInfo) {
-  return model.findByIdAndUpdate(userinfo.openId, { $set: userinfo }, { upsert: true })
+  return model.findByIdAndUpdate(userinfo.openId, { $set: userinfo }, { upsert: true }).exec()
 }
