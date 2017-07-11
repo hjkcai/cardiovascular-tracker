6f414ddf72d2ec26357662605664a78e0740e43a
hjkcai <huajingkun1214@gmail.com>
2017-07-11 10:09:37 +0800

    refactor(client): 简化 TabMixin 写法

--- client/src/mixins/tab.js
+++ client/src/mixins/tab.js
@@ -10,24 +10,22 @@ export default class TabMixin extends wepy.mixin {
 
   methods = {
     switchTab (e) {
-      this.activeTab = typeof e === 'number' ? e : Number(e.currentTarget.dataset.index)
-
-      const activeTab = this.tabs[this.activeTab]
-      if (activeTab && activeTab.component) {
-        try {
-          // 如果组件没有 refresh 函数则会报错
-          this.$invoke(activeTab.component, 'refresh')
-        } catch (err) {}
-      }
+      // 这里调用的是下面的那个 switchTab 函数
+      // 写法有点神奇
+      this.switchTab(e)
     }
   }
 
   switchTab (e) {
-    this.$mixins.forEach(mixin => {
-      if (mixin instanceof TabMixin) {
-        mixin.methods.switchTab.call(this, e)
-      }
-    })
+    this.activeTab = typeof e === 'number' ? e : Number(e.currentTarget.dataset.index)
+
+    const activeTab = this.tabs[this.activeTab]
+    if (activeTab && activeTab.component) {
+      try {
+        // 如果组件没有 refresh 函数则会报错
+        this.$invoke(activeTab.component, 'refresh')
+      } catch (err) {}
+    }
   }
 
   onLoad () {
