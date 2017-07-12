785f6c05c69ca15c6a9c25299f5f5d616b0d5d76
hjkcai <huajingkun1214@gmail.com>
2017-07-12 14:09:51 +0800

    feat(server): 允许校验中间件验证 query

--- server/src/lib/middlewares/validate.ts
+++ server/src/lib/middlewares/validate.ts
@@ -5,11 +5,12 @@ import { ValidationFailureError } from '../errors'
 import { ajv, fromSchema, Schema } from '../ajv'
 
 /** 数据校验中间件 */
-export default function ValidateMiddlewareFactory (schema: Schema) {
+export default function ValidateMiddlewareFactory (schema: Schema, type: 'body' | 'query' = 'body') {
   const validator = ajv.compile(fromSchema(schema))
   return (function ValidateMiddleware (ctx, next) {
-    if (ctx.request.body == null) throw new ValidationFailureError('Empty request body')
-    else if (validator(ctx.request.body)) return next()
+    const data = type === 'body' ? ctx.request.body : ctx.query
+    if (data == null) throw new ValidationFailureError('Empty data')
+    else if (validator(data)) return next()
     else throw new ValidationFailureError(ajv.errorsText(validator.errors))
   }) as Middleware
 }
