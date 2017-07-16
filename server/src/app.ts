cc348524299c2bbaa3edbebb40fbf4d2eb574fb4
hjkcai <huajingkun1214@gmail.com>
2017-07-16 18:56:57 +0800

    feat(server): 使用 rewrite

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,9 +9,13 @@ import AuthMiddleware from './lib/middlewares/auth'
 import ApiResponserMiddleware from './lib/middlewares/api-responser'
 import LoggerMiddleware from './lib/middlewares/logger'
 
+// koa-rewrite 没有 d.ts 定义, 只能这样引入
+const rewrite = require('koa-rewrite')
+
 const app = new Koa()
 app.use(LoggerMiddleware)
 app.use(bodyParser())
+app.use(rewrite(`/ch/*`, '/$1'))
 app.use(ApiResponserMiddleware)
 app.use(AuthMiddleware(/\/login$/))
 app.use(router.prefix('/api').routes())
