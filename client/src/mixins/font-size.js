52394a98dd54afc4317ebe82c7251651e15896d5
hjkcai <huajingkun1214@gmail.com>
2017-07-19 21:12:13 +0800

    feat(client): 添加 FontSizeMixin

---
+++ client/src/mixins/font-size.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import wepy from 'wepy'
+
+export default class FontSizeMixin extends wepy.mixin {
+  data = {
+    fontSize: ''
+  }
+
+  onLoad () {
+    this.fontSize = this.$state.fontSize
+    this.fontSizeChangedHandler = () => {
+      this.fontSize = this.$state.fontSize
+      this.$apply()
+    }
+
+    this.$globalEvents.on('fontSizeChanged', this.fontSizeChangedHandler)
+  }
+}
